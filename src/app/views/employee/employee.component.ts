532cf7e92d9540d7df476663623584ebc8e48341
Yeison Andres Rua Del rio <yecorua@hotmail.com>
2020-07-17 19:21:29 -0500

    Creando Proyecto

---
+++ src/app/views/employee/employee.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service'
+import { EmployeeModel } from 'src/app/interfaces/employee.model';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.sass']
+})
+export class EmployeeComponent implements OnInit {
+  employees: Array<EmployeeModel> = new Array<EmployeeModel>();
+  name: string = "";
+  isLoading: boolean = true;
+
+  constructor(
+    protected _employeeService: EmployeeService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this._employeeService.getAll(this.name).subscribe(resp => {
+      this.employees = resp;
+      this.isLoading = false;      
+    });
+  }
+
+  onSubmit() {
+    this.isLoading = true;
+    this.getAll();
+  }
+}
